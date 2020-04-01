f1da1f665dac90a9b09f4329a5978ec86a3d5a86
fitrianisyukur <fitriani1800016133@webmail.uad.ac.id>
2020-04-01 22:44:38 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './Halaman utama.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fitritekweb133';
-}
+  title = 'fitritekweb133'; 
+ }
